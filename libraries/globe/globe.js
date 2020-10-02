fe70841b62c82e3d05f2e8ea74b3b08741149008
IAMCrazyCat <liuzimingjay@vip.qq.com>
2020-10-02 14:28:20 +1000

    New

--- libraries/globe/globe.js
+++ libraries/globe/globe.js
@@ -127,8 +127,8 @@ DAT.Globe = function (container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    //uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'world.jpg');
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('https://raw.githubusercontent.com/IAMCrazyCat/CrazyCat/master/materials/world.jpg');
+    uniforms['texture'].value = new THREE.TextureLoader().load(imgDir + 'world.jpg');
+    //uniforms['texture'].value = THREE.ImageUtils.loadTexture('https://raw.githubusercontent.com/IAMCrazyCat/CrazyCat/master/materials/world.jpg');
  
     material = new THREE.ShaderMaterial({
 
